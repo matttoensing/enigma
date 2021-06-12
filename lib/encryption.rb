a2ea37004d734ebf1c3462b68f6f55fdf7e283ec
Matt Toensing <matthew.toensing@gmail.com>
2021-06-12 10:50:53 -0600

    Include test for transforming message into an array and method for it

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,4 +1,14 @@
 
 class Encryption
+  attr_reader :message, :offset, :elements
 
+  def initialize(message)
+    @message = message
+    @offset = Offset.new
+    @elements = Alphabet.new.elements
+  end
+
+  def transform_message
+    @message.chars
+  end
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -3,9 +3,27 @@ require_relative "spec_helper"
 RSpec.describe Encryption do
   context 'instantiation' do
     it 'exists' do
-      encrypt = Encryption.new
+      encrypt = Encryption.new("Matt Ryan")
 
       expect(encrypt).to be_an_instance_of(Encryption)
+    end
+
+    it 'has attributes' do
+      encrypt = Encryption.new("Matt Ryan")
+
+      expect(encrypt.message).to eq("Matt Ryan")
+      expect(encrypt.offset).to be_an_instance_of(Offset)
+      expect(encrypt.elements.class).to eq(Array)
+    end
+  end
+
+  context 'methods' do
+    it 'can put message into an array' do
+      encrypt = Encryption.new("Matt Ryan")
+
+      expected = ['M', 'a', 't', 't', ' ', 'R', 'y', 'a', 'n']
+
+      expect(encrypt.transform_message).to eq(expected)
     end
   end
 end
