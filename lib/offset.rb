7d0317a7be3d3eb3fd50ad1d06d3736c94d12a78
Matt Toensing <matthew.toensing@gmail.com>
2021-06-12 12:39:17 -0600

    Remove randomness and date to make encryption easier

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,11 +1,10 @@
 
 class Offset
-  attr_reader :values, :key, :date
+  attr_reader :key, :date
 
-  def initialize
-    @values = ("0".."9").to_a * 5
-    @key = values.sample(5)
-    @date = Time.new.strftime("%d%m%y")
+  def initialize(key, date)
+    @key = key
+    @date = date
   end
 
   def square_date
@@ -18,23 +17,30 @@ class Offset
     x = integers[-4..-1]
   end
 
+  def grouped_key
+    @key.chars
+  end
+
   def a_key
-    num = @key[0..1].join
+    num = grouped_key[0..1].join
     num.rjust(0).to_i + last_four_digits[0]
   end
 
   def b_key
-    num = @key[1..2].join
+    num = grouped_key[1..2].join
     num.rjust(0).to_i + last_four_digits[1]
   end
 
   def c_key
-    num = @key[2..3].join
+    num = grouped_key[2..3].join
     num.rjust(0).to_i + last_four_digits[2]
   end
 
   def d_key
-    num = @key[3..4].join
+    num = grouped_key[3..4].join
     num.rjust(0).to_i + last_four_digits[3]
   end
 end
+
+# = (("0".."9").to_a * 5).sample(5)
+# = Time.new.strftime("%d%m%y")
