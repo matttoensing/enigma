6adeafe5db6ca4bcb9447f83220781d5255057de
Matt Toensing <matthew.toensing@gmail.com>
2021-06-13 11:09:44 -0600

    Change group method and encrypted message to be an array upon instantiation

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -3,8 +3,22 @@ class Decryption
   attr_reader :encryped_message, :offset, :elements
 
   def initialize(encryped_message, offset)
-    @encryped_message = encryped_message
+    @encryped_message = encryped_message.downcase.chars
     @offset = offset
     @elements = ("a".."z").to_a << " "
+  end
+
+  def group
+    @encryped_message.each_slice(4).to_a
+  end
+
+  def shift(value)
+    @elements.rotate(value)
+  end
+
+  def update_index_value(index, num)
+    x = index - num
+    return (x % 27) if x < 0
+    x
   end
 end
