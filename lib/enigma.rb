4be87e96387433aa2a215fd73d0b5f75c179fc1a
Matt Toensing <matthew.toensing@gmail.com>
2021-06-13 19:29:18 -0600

    Update decrypt method and change name of message method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,24 +1,38 @@
 
 class Enigma
-  attr_reader :file_path
+  attr_reader :file_path,
+              :offset,
+              :encryption,
+              :message,
+              :decryption
 
-  def initialize(file_path)
+  def initialize(file_path, offset, encryption, decryption)
     @file_path = file_path
-    @offset = Offset.new("02345", "110621")
-    @encryption = Encryption.new(message, @offset)
+    @offset = offset
+    @encryption = encryption
+    @message = @encryption.message
+    @decryption = decryption
   end
 
-  def message
+  def message_from_file
     holder = File.open(ARGV[0], 'r')
     incoming_message = holder.read
     holder.close
   end
 
-  def encrypt
+  def encrypt(message = @message, key = @offset.key, date = @offset.date)
     {
       encryption: @encryption.message_encrypted.strip,
-      key: @offset.key,
-      date: @offset.date
+      key: key,
+      date: date
+    }
+  end
+
+  def decrypt(message = encrypt[:encryption], key = @offset.key, date = @offset.date)
+    {
+      decryption: @decryption.decrypt,
+      key: key,
+      date: date
     }
   end
 end
