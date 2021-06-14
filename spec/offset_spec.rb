b143516c98581929b1835d26acced9b19cd11773
Matt Toensing <matthew.toensing@gmail.com>
2021-06-13 18:18:24 -0600

    Add stubs for default key and date

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -1,4 +1,5 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
+require 'date'
 
 RSpec.describe Offset do
   context "intantiation" do
@@ -14,6 +15,16 @@ RSpec.describe Offset do
       expect(offset.key).to eq("02345")
       expect(offset.date).to eq("110621")
     end
+
+    it 'has methods for default attributes' do
+      offset = Offset.new
+      allow(offset).to receive(:key).and_return("02345")
+      allow(offset).to receive(:date).and_return("130621")
+
+      expect(offset.key).to eq("02345")
+      expect(offset.date).to eq("130621")
+      expect(offset.random_number_generator.length).to eq(5)
+    end
   end
 
   context 'methods' do
@@ -21,6 +32,13 @@ RSpec.describe Offset do
       offset = Offset.new("02345", "110621")
 
       expect(offset.square_date).to eq(12237005641)
+    end
+
+    xit 'can square the default date' do
+      offset = Offset.new
+      allow(offset).to receive(:date).and_return("120621")
+
+      expect(offset.square_date).to eq(17061845641)
     end
 
     it 'can take the last 4 digits of the square date number' do
@@ -31,6 +49,15 @@ RSpec.describe Offset do
 
     it 'can convert the key into a collection' do
       offset = Offset.new("02345", "110621")
+
+      expected = ["0", "2", "3", "4", "5"]
+
+      expect(offset.grouped_key).to eq(expected)
+    end
+
+    xit 'can convert the default key into a collection' do
+      offset = Offset.new
+      allow(offset).to receive(:key).and_return("02345")
 
       expected = ["0", "2", "3", "4", "5"]
 
