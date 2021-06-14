e2c637c2d9168c2833c8a31405eca7849cfcc7eb
Matt Toensing <matthew.toensing@gmail.com>
2021-06-14 15:56:35 -0600

    Order enigma arguments correctly

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -10,8 +10,17 @@ holder.close
 offset = Offset.new
 encrypt = Encryption.new(message, offset)
 decrypt = Decryption.new(encrypt.message_encrypted, offset)
-enigma = Enigma.new(offset, encrypt, decrypt)
+enigma = Enigma.new(encrypt, decrypt, offset)
 
 writer = File.open(ARGV[1], 'w')
 writer.write(enigma.encrypt[:encryption])
 writer.close
+puts "Created #{ARGV[1]} with the key #{offset.key} and #{offset.date}"
+
+# handle = File.open(ARGV[0], 'r')
+# encrypted_message = handle.read
+# handle.close
+#
+# decrypted_writer = File.open(ARGV[1], 'w')
+# decrypted_writer(enigma.decrypt[:decryption])
+# decrypted_writer.close
