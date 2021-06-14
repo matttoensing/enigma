db7e21fcb6353a3cd1bb00f702468549d15338c3
Matt Toensing <matthew.toensing@gmail.com>
2021-06-14 11:15:37 -0600

    Change pathways for files

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,17 @@
-require 'offset'
-require 'encryption'
-require 'decryption'
-require 'enigma'
+require_relative 'offset'
+require_relative 'encryption'
+require_relative 'decryption'
+require_relative 'enigma'
+
+holder = File.open(ARGV[0], 'r')
+message = holder.read
+holder.close
+
+offset = Offset.new
+encrypt = Encryption.new(message, offset)
+decrypt = Decryption.new(encrypt.message_encrypted, offset)
+enigma = Enigma.new(offset, encrypt, decrypt)
+
+writer = File.open(ARGV[1], 'w')
+writer.write(enigma.encrypt[:encryption])
+writer.close
