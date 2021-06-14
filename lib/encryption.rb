645455bd696c64c6cd031d8cee9ceb515bcfe767
Matt Toensing <matthew.toensing@gmail.com>
2021-06-14 16:37:55 -0600

    Change update methods to include equals, hopefully it fixed bug

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,7 +2,7 @@
 class Encryption
   attr_reader :message, :offset, :elements
 
-  def initialize(message, offset = Offset.new)
+  def initialize(message, offset)
     @message = message.downcase.chars
     @offset = offset
     @elements = ("a".."z").to_a << " "
@@ -18,19 +18,19 @@ class Encryption
 
   def update(index, num)
     x = index + num
-    return (x % 27) if x > 27
+    return (x % 27) if x >= 27
     x
   end
 
   def character(index1, index2)
     if index1 == 0
-      @elements[update(index2, offset.a_key)]
+      @elements[update(index2, offset.get_key(index1))]
     elsif index1 == 1
-      @elements[update(index2, offset.b_key)]
+      @elements[update(index2, offset.get_key(index1))]
     elsif index1 == 2
-      @elements[update(index2, offset.c_key)]
+      @elements[update(index2, offset.get_key(index1))]
     elsif index1 == 3
-      @elements[update(index2, offset.d_key)]
+      @elements[update(index2, offset.get_key(index1))]
     end
   end
 
