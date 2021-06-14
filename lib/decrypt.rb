ca4214779db56581bc07fdf17464d3b2c343ad66
Matt Toensing <matthew.toensing@gmail.com>
2021-06-14 15:58:31 -0600

    Create decrypt runner file and include call commands

---
+++ lib/decrypt.rb
@@ -0,0 +1,18 @@
+require_relative 'offset'
+require_relative 'encryption'
+require_relative 'decryption'
+require_relative 'enigma'
+
+holder = File.open(ARGV[0], 'r')
+message = holder.read
+holder.close
+
+offset = Offset.new(ARGV[2], ARGV[3])
+encrypt = Encryption.new(message, offset)
+decrypt = Decryption.new(message, offset)
+enigma = Enigma.new(encrypt, decrypt, offset)
+
+writer = File.open(ARGV[1], 'w')
+writer.write(decrypt.message_decrypted)
+writer.close
+puts "Created #{ARGV[1]} with the key #{offset.key} and #{offset.date}"
